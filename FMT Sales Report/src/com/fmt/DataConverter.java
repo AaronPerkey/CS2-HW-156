dd20bee96da2627fcfbc52f8cc8743ae270b03f7
Aaron <111891230+AaronPerkey@users.noreply.github.com>
2023-02-22 20:11:01 -0600

    Update DataConverter.java

--- FMT Sales Report/src/com/fmt/DataConverter.java
+++ FMT Sales Report/src/com/fmt/DataConverter.java
@@ -1,5 +1,12 @@
 package com.fmt;
-
+/**
+ * 
+ * Processes a comma-separated value (CSV) file of items, stores, and persons. It will
+ * print the invoices into a nice legible format.
+ * 
+ * @author aaron, emma
+ *
+ */
 public class DataConverter {
 
 }
