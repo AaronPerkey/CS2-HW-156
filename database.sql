9687e3cf0bfbef25625905c75e11c4f96aa1f0de
Aaron <111891230+AaronPerkey@users.noreply.github.com>
2023-03-26 20:41:49 -0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -133,34 +133,3 @@ create table InvoiceItems(
     foreign key (invoiceId) references Invoice(invoiceId),
     foreign key (itemsId) references Items(itemsId)
     );
-   
-   
-
-
-# Queries
-#1. A query to retrieve the main attributes of each person (their code, and last/first name)
-
-#2. A query to retrieve the major fields for every person including their address (but excluding emails)
-
-#3. A query to get the email addresses of a specific person
-
-#4. A query to change the email address of a specific email record
-
-#5. A query (or series of queries) to remove a specific person record
-
-#6. A query to get all the items on a specific invoice record
-
-#7. A query to get all the items purchased by a specific person
-
-#8. A query to find the total number of sales made at each store
-
-#9. A query to find the total number of sales made by each employee
-
-#10. A query to find the subtotal charge of all products in each invoice (hint: you can take an aggregate of a mathematical expression). Do not include taxes.
-
-#11. A query to detect invalid data in invoice as follows. In a single invoice, a particular product should only appear once since any number 
-#    of units can be consolidated to a single record. For example, an invoice should not have two records for fertilizer; one for 10 liters and another 
-#    for 25 liters. Instead, it should have a single record for 35 liters. Write a query to find any invoice that includes multiple instances of the same product. 
-#    If your database design prevents such a situation, you should still write this query (but of course would never expect any results).
-
-#12. Write a query to detect a potential instance of fraud where an employee makes a sale to themselves (the same person is the sales person as well as the customer).
